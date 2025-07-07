e29d012e4cfe439bd6075c16af677f97c4b849e0
Steve Yen <steve.yen@gmail.com>
2025-07-07 15:06:16 -0700

    let VALUE_KINDS

--- src/parser/value-kinds.ts
+++ src/parser/value-kinds.ts
@@ -5,7 +5,7 @@ interface ValueKind {
     description: string;
 }
 
-const VALUE_KINDS: ValueKind[] = [
+let VALUE_KINDS: ValueKind[] = [
     {
         kind: 'datetime',
         pattern: /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d+)?(Z|[+-]\d{2}:\d{2})?$|^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}(\.\d+)?$/,
