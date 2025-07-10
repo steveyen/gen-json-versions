aa5cea4310116ce66d82455488d0a444cf445810
Steve Yen <steve.yen@gmail.com>
2025-07-10 13:17:42 -0700

    Refactor data generation loop to use indexed iteration and introduce generateObjExample method for improved clarity and structure.

--- src/generator/data-generator.ts
+++ src/generator/data-generator.ts
@@ -1,7 +1,7 @@
 // Core data generation engine will be implemented here.
 
 import { EmpDatabase } from '../parser/emp-parser';
-import { Phase } from '../parser/phases-parser';
+import { Phase, CodeBlock } from '../parser/phases-parser';
 
 export class DataGenerator {
     constructor(private phases: Phase[], private emps: EmpDatabase) {
@@ -14,7 +14,9 @@ export class DataGenerator {
 
         const outPhases: any[] = [];
 
-        for (const phase of this.phases) {
+        for (let phaseIndex = 0; phaseIndex < this.phases.length; phaseIndex++) {
+            const phase = this.phases[phaseIndex];
+
             const outColls: Record<string, any> = {};
 
             for (const jsonBlock of phase.jsonBlocks) {
@@ -25,8 +27,14 @@ export class DataGenerator {
 
                     const outObjs = outColls[collName] || [];
 
-                    for (const objExample of collExamples as any[]) {
+                    const collExamplesArr = collExamples as any[];
+
+                    for (let i = 0; i < collExamplesArr.length; i++) {
+                        const collExample = collExamplesArr[i];
+
                         for (let i = 0; i < numExamples; i++) {
+                            let objExample = this.generateObjExample(phaseIndex, phase, jsonBlock, collName, collExamplesArr, i, collExample);
+
                             outObjs.push(objExample);
                         }
                     }
@@ -42,5 +50,9 @@ export class DataGenerator {
         }
 
         return outPhases;
+    }
+
+    private generateObjExample(phaseIndex: number, phase: Phase, jsonBlock: CodeBlock, collName: string, collExamples: any[], i: number, collExample: any): any {
+        return collExample;
     }
 }
